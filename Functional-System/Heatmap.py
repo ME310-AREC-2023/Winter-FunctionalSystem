5d5e5c802a5a3e1073c49140ff549913d8504366
Josh DeWitt <dotdash32@gmail.com>
2023-03-06 20:55:05 -0800

    chore|doc: remove unused code, add comments

--- Functional-System/Heatmap.py
+++ Functional-System/Heatmap.py
@@ -8,37 +8,19 @@ import numpy
 from simpleimage import SimpleImage
 
 
-def circle(n, r, x0, y0, s, var):
-    """
-    >>> r = 10
-    >>> x0 = 15
-    >>> n = 2
-    >>> y0 = 20
-    >>> s = 4
-    >>> var = 0
-    >>> circle(n, r, x0, y0, s, var)
-
-    """
-
-    source = range(n)
-    delta = (2 * r)/(n - 1)
-    xlist_top = list(map((lambda x: round((delta * x)) - r + x0), source))
-    xlist_bottom = list(reversed(xlist_top))
-    ylist_top = list(map((lambda xl: round(math.sqrt(-(xl-x0)**2 + r**2) + y0) + numpy.random.randint(-s, s)), xlist_top))
-    ylist_bottom = list(map((lambda xl: round(-math.sqrt(-(xl - x0)**2 + r**2) + y0) + numpy.random.randint(-s, s)), xlist_bottom))
-    xlist = xlist_top + xlist_bottom
-    ylist = ylist_top + ylist_bottom
-    points = []
-    for i in range(2*n):
-        points.append(((numpy.random.randint(var, 3*var)), xlist[i], ylist[i]))
-    return points
-
 
 def z_amp(x, y, s, mx, my):
     """
-    >>> z_amp(1,1,1,1,1)
-    1.0
+    Calculate a "heat" based on location.
+
+    x,y is current location (calcs distance to base point)
+    mx, my is base point, center of the hotspot
+    s - standard deviation? implies how wide the spread is
     """
+    # """
+    # >>> z_amp(1,1,1,1,1)
+    # 1.0
+    # """
     mag = 0.3 * 255/(s**2)
     z = round(mag*math.exp(-((mx - x)**2 + (my - y)**2)/(2*(30*s)**2)))
     return z
@@ -53,9 +35,12 @@ def z_radius(s):
 
 def rgb(minimum, maximum, value):
     """
-    >>> rgb(0, 255, 125)
-
+    Rescale value for heat map coloring
     """
+    # """
+    # >>> rgb(0, 255, 125)
+
+    # """
     minimum, maximum = float(minimum), float(maximum)
     ratio = 2 * (value-minimum) / (maximum - minimum)
     b = int(max(0, 255*(.9 - ratio)))
@@ -74,21 +59,9 @@ def main():
     w = 1000
     h = 1000
     out = SimpleImage.blank(w, h, 'black')
-    # points = [(5, 30, 50), (3, 50, 20), (1, 90, 90), (2, 75, 80)]
-    # n = 10
-    # r = 20
-    # x0 = 50
-    # y0 = 50
-    # sigma = 3
-    # var = 3
-    # points1 = circle(n, r, x0, y0, sigma, var)
-    # r = 10
-    # points2 = circle(n, r, 12, 18, sigma, var)
-    # points3 = circle(n, 7, 64, 64, sigma, var)
-    # points4 = circle(n, 15, 45, 64, sigma, var)
-    # points5 = circle(n, 2, 75, 10, sigma, var)
-    # points = points1 + points2 + points3 + points4 + points5
     points = []
+
+    # open the file, read in the positions
     with open("positions.txt") as f:
         for line in f:
             linex_extract = line.split("x=")
@@ -99,7 +72,6 @@ def main():
             q = 100*1/int(liney_extract[1][:2])
             points.append((q, x_p, y_p))
 
-    imgs = []
     out = SimpleImage.blank(w,h, 'black') # create an empty image
 
     for point in points:
